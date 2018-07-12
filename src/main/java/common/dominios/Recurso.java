8f237c8fc90b0c426f96c6bb2fe0de5d2ed37c1c
“Emilio” <emilio.jeldes@gmail.com>
2018-07-12 13:18:14 -0400

    script sql funcionando con todos los parametros de las clases para mapear

--- src/main/java/common/dominios/Recurso.java
+++ src/main/java/common/dominios/Recurso.java
@@ -2,21 +2,20 @@ package common.dominios;
 
 import common.dominios.enums.TipoRecurso;
 import common.dominios.enums.TipoTexto;
-import java.io.Serializable;
-import java.util.List;
 import lombok.Data;
 
+import java.io.Serializable;
+import java.util.List;
+
 @Data
-public class Recurso implements Serializable {
+public class Recurso<T> implements Serializable {
 
     private static final long serialVersionUID = -7684949284722148086L;
 
     private Integer id;
     private String titulo;
     private int totalPaginas;
-    private Libro libro;
-    private Periodico periodico;
-    private Revista revista;
+    private T recurso;
     private TipoRecurso tipoRecurso;
     private List<Autor> autores;
     private Editorial editorial;
